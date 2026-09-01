528d5aede34cfcdee44e5185222a276f9a8b63fa
OneNov0209 <95606017+OneNov0209@users.noreply.github.com>
2026-09-01 22:01:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
+  tanstackStart: {
+    server: { entry: "server" },
   },
 });
